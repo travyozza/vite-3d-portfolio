5f81c04400679c3c963c621004ef4842c6eb10e2
travyozza <84102421+travyozza@users.noreply.github.com>
2024-11-25 15:40:37 +1300

    render progress added

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,7 +10,20 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
@@ -18,7 +31,7 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return (
     <Canvas
-      frameLoop="demand"
+      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
